7a01f8973a437d1d302a8232a7eeb483293aae47
SheikLicFusion <sheik_lic@protonmail.com>
2025-08-24 08:14:35 -0500

    Create script.js
    añadir interactividad a tu página

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("¡PlataformaSinergia cargada con éxito! La sinergia de mentes ha comenzado.");
+
+// Ejemplo de una función simple que podría activarse con un botón
+function mostrarMensajeBienvenida() {
+    alert("¡Bienvenido a la Sinergia!");
+}
